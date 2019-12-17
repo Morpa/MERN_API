59eb816e042b7e0ee8289d414bc8014421db2c56
André Morpanini <andremorpanini@MacBook-Pro-de-Andre.local>
2019-12-17 14:51:06 +0000

    Add Cors

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
@@ -29,7 +39,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://morpa:CeXFj5oDt7ua1Gh3@mern-3pduj.azure.mongodb.net/places?retryWrites=true&w=majority"
+    "mongodb+srv://morpa:CeXFj5oDt7ua1Gh3@mern-3pduj.azure.mongodb.net/mern?retryWrites=true&w=majority"
   )
   .then(() => {
     app.listen(5000);
